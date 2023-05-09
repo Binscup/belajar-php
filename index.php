3c0fd2fa06e0d9bd377f55e2a69e8aab28b8618a
SMK TI BAZMA 10 <bintang684@smk.belajar.id>
2023-05-09 08:51:56 +0700

    uploud php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar php</title>
+</head>
+
+<body>
+    
+<?php
+//tahap 1
+    echo"Belajar php";
+    echo "/n";
+    
+//tahap 2
+$name = "Belajar php menggunakan var";
+echo $name;
+?>
+</body>
+</html>
