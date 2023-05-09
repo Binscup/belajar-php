2386a7b119c15cbd0c39bf40d620f74a32000131
SMK TI BAZMA 10 <bintang684@smk.belajar.id>
2023-05-09 14:37:14 +0700

    mengubah explode

---
+++ form/define.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+define("fungsi","ini adalah ctakan hasil const dari fungsi");
+?>
+<br>
+<?php
+//define const array 
+define ("arrayKelas", ["kelas x", "kelas xi", "kelas xii"]);
+echo arrayKelas;
+?>
+
+</body>
+</html>
---
+++ form/explode.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// penjumlahan
+// pakai cara 1
+echo 5+5 .'<br>';
+// pakai cara 2
+$a =10;
+$b = 5;
+echo $a + $b;
+?>
+<br>
+<?php
+// pangkat
+$angka1 = 5;
+$angka2 = 4;
+echo pow($angka1, $angka1)
+?>
+</body>
+</html>
